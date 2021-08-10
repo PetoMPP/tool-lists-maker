6133755a1b742cb35bfdd1e18f80b9bb72204d24
PetoMPP <piotreq22@gmail.com>
2021-08-10 17:59:39 +0200

    exceptions in python script

--- Python/mainapp.py
+++ Python/mainapp.py
@@ -18,13 +18,21 @@ NCpath = input("Podaj ścieżkę do folderu z programami (np. M:\3_Retransmissio
 dirs = dirmod.getDir(NCpath) #lista folderów
 i = 0
 while i < len(dirs):
-    NCfiles = dirmod.getfiles(NCpath+"/"+dirs[i]) #lista plików w folderze
+    try:
+        NCfiles = dirmod.getfiles(NCpath+"/"+dirs[i]) #lista plików w folderze
+    except FileNotFoundError:
+        print("Podfolder w podfolderze!!")
     j = 0
     while j < len(NCfiles):
         tname = dirs[i]+"-"+NCfiles[j] #nazwa pliku
         tdir = dirs[i]+"/"+NCfiles[j] #ścieżka do pliku
         tfile = open("Lista"+tname+".txt", "w") #stworzenie pliku numer-mocx
-        tlist = toolgetmod.fileTlist(NCpath+"/"+tdir) #lista numerów T w pliku
+        try:
+            tlist = toolgetmod.fileTlist(NCpath+"/"+tdir) #lista numerów T w pliku
+        except UnicodeDecodeError:
+            print("Zły format pliku!!")
+        except FileNotFoundError:
+            print("Podfolder w podfolderze!!")
         k = 0
         while k < len(tlist):
             tfile.write(tlist[k]+"\n") #wpisanie narzędzi do plików
