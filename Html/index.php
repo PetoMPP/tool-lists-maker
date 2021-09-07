65de5461e1e2242a803bfc2455b017e99ce1ab01
PetoMPP <88336224+PetoMPP@users.noreply.github.com>
2021-09-07 18:40:38 +0200

    Delete Html directory

--- Html/index.php
+++
@@ -1,6 +0,0 @@
-<?php echo file_get_contents("index.html"); ?> 
-<footer>
-    <div class="wrap row small-up-1 medium-up-3" style="height:100px;">
-        <p style="font-size: 0.875em; color:DarkGrey; text-align:right; padding-top: 80px; padding-right: 50px">Copyright © PetoMPP <?php echo date("Y"); ?></p>
-    </div>
-</footer>
